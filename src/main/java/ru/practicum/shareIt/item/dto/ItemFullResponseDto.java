d02d39d9ef01f48d8ca5d7d0393e329917b56f94
Artem <a.ermizin.corp@yandex.ru>
2026-06-07 20:07:03 +0700

    Fix checkstyle

--- src/main/java/ru/practicum/shareIt/item/dto/ItemFullResponseDto.java
+++ src/main/java/ru/practicum/shareIt/item/dto/ItemFullResponseDto.java
@@ -3,7 +3,7 @@ package ru.practicum.shareIt.item.dto;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record ItemFullResponseDto (Long id, String name, String description,
+public record ItemFullResponseDto(Long id, String name, String description,
                                    Boolean available, LocalDateTime lastBooking, LocalDateTime nextBooking,
                                    List<CommentResponseDto> comments, Long request) {
 }
--- src/main/java/ru/practicum/shareIt/item/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareIt/item/dto/ItemRequestDto.java
@@ -4,9 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Data
 public class ItemRequestDto {
         @NotBlank(message = "Название товара не может быть null или пустым.")
